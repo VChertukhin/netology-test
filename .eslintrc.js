473d812f07c5a7309e2d6fe77864693f8241bfce
VChertukhin <VChertukhin@protonmail.com>
2020-11-02 17:12:37 +0300

    initital commit (just prepared all what I need)

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+
+module.exports = {
+    root: true,
+    plugins: [
+        '@typescript-eslint',
+    ],
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    parserOptions: {
+        sourceType: 'module',
+        project: [
+            './tsconfig.json',
+        ],
+        tsconfigRootDir: __dirname,
+        warnOnUnsupportedTypeScriptVersion: false,
+    },
+    ignorePatterns: [
+        'node_modules/*',
+        'dist/*',
+        'lib/*',
+    ],
+    overrides: [
+        {
+            files: [`**/*.ts`, `**/*.tsx`],
+        },
+    ],
+    rules: {
+        semi: [2, 'always'],
+        quotes: [2, 'single'],
+    }
+};
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+/* eslint-disable */
+const path = require('path');
+
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const __DEV__ = process.env.NODE_ENV === 'development';
+
+const context = path.resolve(__dirname, 'src');
+
+const getAlias = () => {
+    const aliasObj = { "@": "." };
+
+    const aliasList = [
+        "interfaces",
+        "components",
+        "services",
+    ];
+    // resolve "@interfaces", etc.
+    aliasList.map(aliasName => { aliasObj[`@${aliasName}`] = path.resolve(context, aliasName) });
+
+    return aliasObj;
+};
+
+module.exports = {
+    context,
+    entry: './index.tsx',
+    devtool: __DEV__ ? 'source-map' : false,
+    devServer: {
+        port: 5000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        alias: getAlias(),
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.[hash:8].js',
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./index.html",
+            filename: "index.html",
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{ from: './assets/favicon.ico', to: 'favicon.ico' }],
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    watch: true,
+};
