899080a0860236efd3e409ebe61db7b8cad5ea0b
VChertukhin <VChertukhin@protonmail.com>
2020-11-04 18:09:25 +0300

    selected ratio

--- src/components/UsersListHeader.tsx
+++ src/components/UsersListHeader.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import { useSelector } from 'react-redux';
 
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -6,9 +7,15 @@ import ListItemText from '@material-ui/core/ListItemText';
 import AppBar from '@material-ui/core/AppBar';
 import Checkbox from '@material-ui/core/Checkbox';
 
+import { selectUsersListLength, selectCheckedUsersNumber } from '@redux/selectors';
 import { ContainerMedium } from '@components';
 
 const UsersListHeader: FunctionComponent = () => {
+    const usersListLength = useSelector(selectUsersListLength);
+    const checkedUsersNumber = useSelector(selectCheckedUsersNumber);
+
+    const selectedNumberRatio = `(${checkedUsersNumber}/${usersListLength})`;
+
     return (
         <AppBar position="fixed" color="default">
             <ContainerMedium>
@@ -22,7 +29,7 @@ const UsersListHeader: FunctionComponent = () => {
                         />
                     </ListItemIcon>
 
-                    <ListItemText primary="Select all" />
+                    <ListItemText primary={`Select all ${selectedNumberRatio}`} />
 
                     <ListItemText
                         primary={'Netology test App'}
